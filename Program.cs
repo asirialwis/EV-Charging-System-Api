7fd825cef9858c9428765c208ee89d131d82d555
asirialwis <asirichathurika900@gmail.com>
2025-10-06 08:35:22 +0530

    build config

--- Program.cs
+++ Program.cs
@@ -99,11 +99,13 @@ builder.Services.AddHealthChecks()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
